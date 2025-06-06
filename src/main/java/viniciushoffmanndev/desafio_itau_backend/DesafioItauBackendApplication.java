f7d77e5d83c260726771f6df96aedec4f6700d86
viniciushoffmanndev <viniciushoffmanndeveloper@gmail.com>
2025-06-06 14:49:06 -0300

    First commit: Ajustando pom.xml para testes junit
    d

---
+++ src/main/java/viniciushoffmanndev/desafio_itau_backend/DesafioItauBackendApplication.java
@@ -0,0 +1,13 @@
+package viniciushoffmanndev.desafio_itau_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioItauBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioItauBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/viniciushoffmanndev/desafio_itau_backend/DesafioItauBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package viniciushoffmanndev.desafio_itau_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioItauBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
