6b8a22e3c5cae20358df330d7f908b704603b84d
viniciushoffmanndev <viniciushoffmanndeveloper@gmail.com>
2025-06-07 12:46:53 -0300

    Seventh commit: Classe TransactionController - anotação web.bind.Annotation

---
+++ src/main/java/viniciushoffmanndev/desafio_itau_backend/controller/TransactionController.java
@@ -0,0 +1,38 @@
+package viniciushoffmanndev.desafio_itau_backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import viniciushoffmanndev.desafio_itau_backend.dto.TransactionRequest;
+import viniciushoffmanndev.desafio_itau_backend.model.Transaction;
+import viniciushoffmanndev.desafio_itau_backend.service.TransactionService;
+
+import java.time.OffsetDateTime;
+
+@RestController
+@RequestMapping("/transacao")
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    private TransactionController(TransactionService transactionService){
+        this.transactionService = transactionService;
+    }
+    @PostMapping
+    public ResponseEntity<Void> create(TransactionRequest request){
+        if (request.getDataHora().isAfter(OffsetDateTime.now()) ||
+        request.getValor() <=0){
+            return ResponseEntity.unprocessableEntity().build();
+        }
+        transactionService.addTransaction(new Transaction(request.getValor(), request.getDataHora()));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @DeleteMapping
+    public ResponseEntity<Void> clear(){
+        transactionService.clearTransaction();
+        return ResponseEntity.ok().build();
+    }
+}
